a820303bc236d71b19e593af1420081eaf36bed4
casserni <nicholascassera@gmail.com>
2020-10-18 19:00:21 -0400

    add typescript

---
+++ lib/index.ts
@@ -0,0 +1,27 @@
+class Exchange {
+  constructor() {
+    // TODO
+  }
+
+  sync(fileName: string) {
+    // TODO
+  }
+
+  buy(quantity: number, price: number) {
+    // TODO
+  }
+
+  sell(quantity: number, price: number) {
+    // TODO
+  }
+
+  getQuantityAtPrice(price: number) {
+    // TODO
+  }
+
+  getOrder(id: string) {
+    // TODO
+  }
+}
+
+module.exports = Exchange;
