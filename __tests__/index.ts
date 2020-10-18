c877d1b96c34552f28fd6d703a281d053fbaca1a
casserni <nicholascassera@gmail.com>
2020-10-18 19:02:11 -0400

    add jest for testing

---
+++ __tests__/index.ts
@@ -0,0 +1,9 @@
+import "jest-expect-message";
+
+describe("Exchange", () => {
+  describe("sync()", () => {});
+  describe("buy()", () => {});
+  describe("sell()", () => {});
+  describe("getQuantityAtPrice()", () => {});
+  describe("getOrder()", () => {});
+});
